22713310448fd373406935f070349d3852c2ace8
kpitsiakkos <pitsiakkoskonstantinos@gmail.com>
2026-03-30 10:06:46 +0100

    Create and run the script for the front end of the tool also add the picture of youtube logo

---
+++ YouTube Script Writing Tool/app.py
@@ -0,0 +1,38 @@
+import gradio as gr
+
+
+
+def generate(prompt, video_length, creativity, api_key):
+    if not api_key:
+        return "Ooopssss!!! Please provide API key.....", "", ""
+
+   
+
+with gr.Blocks(title="YouTube Script Writing Tool") as demo:
+    gr.Markdown("# ❤️ YouTube Script Writing Tool")
+
+    with gr.Sidebar():
+        gr.Markdown("## 😎🗝️")
+        api_key = gr.Textbox(label="What's your API key?", type="password")
+        gr.Image("./Youtube.jpg", show_label=False)
+
+    # Captures User Inputs
+    prompt = gr.Textbox(label="Please provide the topic of the video")
+    video_length = gr.Textbox(label="Expected Video Length 🕒 (in minutes)")
+    creativity = gr.Slider(label="Creativity limit ✨ - (0 LOW || 1 HIGH)", minimum=0.0, maximum=1.0, value=0.2, step=0.1)
+
+    submit = gr.Button("Generate Script for me")
+
+    # Outputs
+    title_output = gr.Textbox(label="Title 🔥")
+    script_output = gr.Textbox(label="Your Video Script 📝", lines=15)
+    search_output = gr.Textbox(label="DuckDuckGo Search Results 🔍", lines=5)
+
+    submit.click(
+        fn=generate,
+        inputs=[prompt, video_length, creativity, api_key],
+        outputs=[title_output, script_output, search_output]
+    )
+
+if __name__ == "__main__":
+    demo.launch()
